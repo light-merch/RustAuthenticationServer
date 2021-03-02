7c54501f9fe5e1d508290fe0e71aa19cb7c83811
justdprroz <dmitri.proskuriakov@gmail.com>
2021-03-02 18:45:58 +0300

    Created authentication server on Python

---
+++ server.py
@@ -0,0 +1,46 @@
+import flask
+import json
+import uuid
+import hashlib
+
+app = flask.Flask(__name__)
+
+users = dict()
+userspath = "users.json"
+try:
+    with open(userspath, "r") as usersfile:
+        users = json.load(usersfile)
+except:
+    pass
+
+@app.route("/")
+def root():
+    return "Authentication server is running!"
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    data = flask.request.form.to_dict()
+    users[data["username"]] = dict({"passwd": hashlib.sha256(bytearray(data["passwd"], "UTF-8")).hexdigest() , "email": data["email"], "auth": -1})
+    with open(userspath, "w") as usersfile:
+        print(json.dump(users, usersfile, indent=4))
+    return "ok"
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    data = flask.request.form.to_dict()
+    print(data)
+    if(users[data["username"]]["passwd"] == hashlib.sha256(bytearray(data["passwd"], "UTF-8")).hexdigest()):
+        if(users[data["username"]]["auth"] == -1):
+            users[data["username"]]["auth"] = uuid.uuid4().hex
+        return users[data["username"]]["auth"]
+    return -1
+
+@app.route("/logout", methods=["GET", "POST"])
+def logout():
+    data = flask.request.form.to_dict()
+    print(users[data["username"]]["auth"], data["auth"])
+    if(users[data["username"]]["auth"] == data["auth"]):
+        users[data["username"]]["auth"] = -1
+    return "ok"
+
+app.run(host="192.168.1.40", port="40500", ssl_context=("ssl/server.cert", "ssl/server.key"))
